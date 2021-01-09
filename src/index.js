44b61ded46d0e91e97680e4b19e0251754a79160
Faiz Azmi Rekatama <faizkort@gmail.com>
2021-01-09 10:20:42 +0700

    setup prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, ThemeProvider } from "@emotion/react";
+
+import "./normalize.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./constants/theme";
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokeapi.vercel.app/api/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
